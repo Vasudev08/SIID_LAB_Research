3817a91a29504a85f6ce4f407af75f825c351f84
ighedosadt-tamu <ighedosadt@tamu.edu>
2025-03-23 21:18:34 -0500

    Viewpoint Transition 1.2 (Zooming In)
    Zooming in phase is working well in the current state. With this the entire transition system is in a complete state. Will need to test a bit more to see how well it works.

--- XR_Interaction/Assets/Scripts/Zoom/MixedOrbitandZoom.cs
+++ XR_Interaction/Assets/Scripts/Zoom/MixedOrbitandZoom.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class MixedOrbitandZoom : MonoBehaviour
@@ -12,28 +13,31 @@ public class MixedOrbitandZoom : MonoBehaviour
     public float zoomOutDuration = 1.0f;
     public float orbitDuration   = 1.0f;
     public float zoomInDuration  = 1.0f;
-    public float rotateSpeed = 10f;
+
 
     // Public entry point for external scripts:
     public void TransitionToViewpoint(Viewpoint target_viewpoint)
     {
+        target_viewpoint.button.interactable = false;
         StartCoroutine(MixedOrbitAndZoomRoutine(userViewpoint, target_viewpoint));
     }
 
     private IEnumerator MixedOrbitAndZoomRoutine(Viewpoint from_viewpoint, Viewpoint target_viewpoint)
     {
+
         Viewpoint lca = FindLeastCommonAncestor(from_viewpoint, target_viewpoint);
 
         
         yield return StartCoroutine(ZoomOutPhase(lca));
 
-        yield return StartCoroutine(OrbitPhase(target_viewpoint));
+        yield return StartCoroutine(OrbitPhase(lca, target_viewpoint));
 
         // 4) Zoom In to the target viewpoint
-       //yield return StartCoroutine(ZoomInPhase(target_viewpoint));
+        yield return StartCoroutine(ZoomInPhase(target_viewpoint));
 
         // Update current viewpoint reference
         userViewpoint = target_viewpoint;
+        target_viewpoint.button.interactable = true;
     }
 
     private IEnumerator ZoomOutPhase(Viewpoint lca)
@@ -44,9 +48,17 @@ public class MixedOrbitandZoom : MonoBehaviour
         Vector3 start_position = userCamera.position;
         Quaternion start_rotation = userCamera.rotation;
 
-        Vector3 vantage_position = lca.pivot.position + lca.cameraOffsetPosition;
+        // Get radius of the circular arc that the LCA LoS is at. This is based on where the intended position of the camera at the LoS is at.
+        float distance = Vector3.Distance(lca.pivot.localPosition, lca.pivot.localPosition + lca.cameraOffsetPosition);
+        Vector3 direction_to_camera = (userCamera.position - lca.pivot.position).normalized;
+        Debug.Log("Zoom out distance: " + distance);
+        
+        Vector3 vantage_position = lca.pivot.position + direction_to_camera * distance;
+        Vector3 x = userCamera.position - lca.pivot.position;
+        Debug.Log("Vantage Point: " + x +  " " + lca.pivot.position + " " + userCamera.position + " " + vantage_position);
         Quaternion vantage_rotation = lca.cameraOffsetRotation;
         float elapsed_time = 0f;
+        
         while (elapsed_time < zoomOutDuration)
         {
             elapsed_time += Time.deltaTime;
@@ -59,25 +71,34 @@ public class MixedOrbitandZoom : MonoBehaviour
             userCamera.position = Vector3.Lerp(start_position, vantage_position, t);
 
             // Rotate camera
-            userCamera.rotation = Quaternion.Slerp(start_rotation, vantage_rotation, t);
+            // userCamera.rotation = Quaternion.Slerp(start_rotation, vantage_rotation, t);
 
             yield return null;
         }
+        Debug.Log("Finished Zoom out for: " + userViewpoint);
     }
 
-    private IEnumerator OrbitPhase(Viewpoint target_viewpoint)
+    private IEnumerator OrbitPhase(Viewpoint lca, Viewpoint target_viewpoint)
     {
         Quaternion start_rotation = userCamera.rotation;
 
         Vector3 start_position = userCamera.position;
-        float radius = Vector3.Distance(userCamera.position, target_viewpoint.pivot.localPosition);
-        Vector3 direction_to_look = (target_viewpoint.pivot.position - userCamera.position).normalized; // view direction the camera needs to match to
-        Quaternion end_rotation = Quaternion.identity;
-
-        Vector3 end_position = target_viewpoint.pivot.position + (target_viewpoint.pivot.forward * radius);
-
+        // Distance from the lca pivot to the camera currently after zooming out.
+        float radius = Vector3.Distance(userCamera.position, lca.pivot.position);
+        
+        // Final target position at the target viewpoint/LoS (***Not the end position to orbit to***)
+        Vector3 position_at_target = target_viewpoint.pivot.position + target_viewpoint.cameraOffsetPosition;
+        Vector3 direction_to_target = (position_at_target - lca.pivot.position).normalized; // Direction to the final target position at the target viewpoint
+        Debug.Log("Orbit distance " + radius);
+        Quaternion end_rotation = quaternion.identity;
+        if (end_rotation.eulerAngles == Vector3.zero)
+        {
+            end_rotation = Quaternion.identity; // Set it to the identity. If it's just 0,0,0 in the inspector it will just snap instead of lerping for some reason
+        }
+        Vector3 end_position = lca.pivot.position + direction_to_target * radius;
+        Debug.Log("End Pos: " + end_position + " Target " + target_viewpoint.pivot.position + " " + target_viewpoint.pivot.position + target_viewpoint.cameraOffsetPosition);
         float elapsed_time = 0f;
-        Debug.Log(Quaternion.Dot(userCamera.rotation, end_rotation));
+        Debug.Log("Rotation: " + userCamera.rotation + " Target Rot: " + target_viewpoint.cameraOffsetRotation + " " +Quaternion.Dot(userCamera.rotation, end_rotation));
         while (elapsed_time < orbitDuration)
         {   
             elapsed_time += Time.deltaTime;
@@ -88,8 +109,39 @@ public class MixedOrbitandZoom : MonoBehaviour
             yield return null;
            
         }
-        Debug.Log("Finished orbit for: " + target_viewpoint.name);
+        Debug.Log("Finished orbit for target: " + target_viewpoint.name);
         
+    }
+
+    private IEnumerator ZoomInPhase(Viewpoint target_viewpoint)
+    {
+        Vector3 start_scale = modelRoot.localScale;
+        Vector3 start_position = userCamera.position;
+        Quaternion start_rotation = userCamera.rotation;
+
+        // Vector3 end_position = target_viewpoint.pivot.position + target_viewpoint.cameraOffsetPosition;
+        Vector3 end_scale = Vector3.one * target_viewpoint.levelOfScale;
+        Quaternion end_rotation = target_viewpoint.cameraOffsetRotation;
+
+
+
+        float elapsed_time = 0f;
+        while (elapsed_time < zoomInDuration)
+        {
+            Vector3 new_camera_offset_position = target_viewpoint.pivot.TransformPoint(target_viewpoint.cameraOffsetPosition);
+            Debug.Log(new_camera_offset_position);
+            new_camera_offset_position = new_camera_offset_position - target_viewpoint.pivot.position;
+            Vector3 end_position = target_viewpoint.pivot.position + new_camera_offset_position;
+
+            elapsed_time += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed_time / zoomInDuration);
+            modelRoot.localScale = Vector3.Lerp(start_scale, end_scale, t);
+            userCamera.position = Vector3.Lerp(start_position, end_position, t);
+            userCamera.rotation = Quaternion.Slerp(start_rotation, end_rotation, t);
+            yield return null;
+        }
+
+        Debug.Log("Finished zooming in for target: " + target_viewpoint.name);
     }
 
     private Viewpoint FindLeastCommonAncestor(Viewpoint current_viewpoint, Viewpoint target_viewpoint)
--- XR_Interaction/Assets/Scripts/Zoom/Viewpoint.cs
+++ XR_Interaction/Assets/Scripts/Zoom/Viewpoint.cs
@@ -1,17 +1,24 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Viewpoint : MonoBehaviour
 {
     public Viewpoint parent;
+    public Button button;
     public List<Viewpoint> childrenViewpoints;
 
     [Header("Transform Data for This Level of Scale")]
     public float levelOfScale = 1f;            // Scale for the entire model at this LoS
     public Transform pivot;       // Transform/pivot of this LoS. For example, center of whole body
     
+    // Transform the viewpoint will be at the target viewpoint based on the pivot
     public Vector3 cameraOffsetPosition;
     public Quaternion cameraOffsetRotation;
+
+    
+    // Transform the viewpoint will zoom out to for the zoom out phase
+
 
     // Auto-update child-parent relationships when viewed in inspector
     private void OnValidate()
